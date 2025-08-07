9441b844a5b69842f70fe1fd6465140684b1bd41
y-bla <yannibarros2709@gmail.com>
2025-08-07 23:03:05 +0000

    trunfo nivel aventureiro

--- trunfo.c
+++ trunfo.c
@@ -8,6 +8,8 @@ int pop1 = 0;
 float km1 = 0.0;
 float pib1 = 0.0;
 int tur1 = 0;
+float denpop1 =0.0;
+float pibcap1 = 0.0;
 char est2 [20];
 char codigo2 [3];
 char nome2 [22];
@@ -15,6 +17,8 @@ int pop2 = 0;
 float km2 = 0.0;
 float pip2 = 0.0;
 int tur2 = 0;
+float denpop2 = 0.0;
+float pibcap2 = 0.0;
 
     printf("Insira as informações sobre as cartas,\nQual é o estado da carta 1?\n");
         scanf("%s", &est1);
@@ -31,6 +35,7 @@ int tur2 = 0;
     printf("Quantos pontos turistícos a carta 1 possui?\n");
         scanf("%d", &tur1);
     printf("\n");
+        denpop1 = pop1 / km1;
 
             printf("Carta 1\n");
             printf("Estado: %s\n", est1);
@@ -40,6 +45,7 @@ int tur2 = 0;
             printf("Área em km2: %f km2\n", km1);
             printf("PIB: %f\n", pib1);
             printf("Pontos turísticos: %d\n", tur1);
+            printf("Densidade populacional: %f", denpop1);
             printf("\n");
 
     printf("Insira as informações sobre as cartas,\nQual é o estado da carta 2?\n");
@@ -57,6 +63,7 @@ int tur2 = 0;
     printf("Quantos pontos turistícos a carta 2 possui?\n");
         scanf("%d", &tur2);
     printf("\n");
+        denpop2 = pop2 / km2;
 
             printf("Carta 2\n");
             printf("Estado: %s\n", est2);
@@ -66,6 +73,7 @@ int tur2 = 0;
             printf("Área em km2: %f km2\n", km2);
             printf("PIB: %f\n", pip2);
             printf("Pontos turísticos: %d\n", tur2);
+            printf("Densidade populacional: %f", denpop2);
             printf("\n");
 
 
